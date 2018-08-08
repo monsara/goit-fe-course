69128f9877aa70ff3fdb8a6df8d16abd3cac2f34
Viktor Hirenko <viktor.hirenko@gmail.com>
2018-08-08 11:57:21 +0300

    add homework: document object model

---
+++ javascript/module-07/js/index.js
@@ -0,0 +1,51 @@
+/*
+  1. Модифицируйте готовую функцию createPostCard() из задания 
+    номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так, 
+    чтобы она принимала объект post с данными для заполнения полей 
+    в карточке.
+      
+  2. Создайте функцию createCards(posts), которая принимает массив
+    объектов-карточек (js/posts.js), вызывает функцию createPostCard(post) столько
+    раз, сколько объектов в массиве, сохраняя общий результат и возвращает 
+    массив DOM-элементов всех постов.
+    
+  3. Повесьте все посты в какой-то уже существующий DOM-узел.
+*/
+
+'use strict';
+
+const createPost = ({ img, title, text, link }) => {
+  const post = document.createElement('div');
+  post.classList.add('post');
+
+  const image = document.createElement('img');
+  image.classList.add('post__image');
+  image.setAttribute('src', img);
+  image.setAttribute('alt', '');
+
+  const head = document.createElement('h2');
+  head.classList.add('post__title');
+  head.textContent = title;
+
+  const descr = document.createElement('p');
+  descr.classList.add('post__text');
+  descr.textContent = text;
+
+  const btn = document.createElement('a');
+  btn.classList.add('button');
+  btn.textContent = 'Read more';
+  btn.setAttribute('href', link);
+
+  post.append(image, head, descr, btn);
+
+  return post;
+}
+
+const createPostCard = posts => {
+  const elements = posts.map(post => createPost(post));
+
+  return elements;
+};
+
+const element = document.querySelector('.card-list');
+element.append(...createPostCard(posts));
---
+++ javascript/module-07/js/posts.js
@@ -0,0 +1,23 @@
+const posts = [
+  {
+    img: 'https://placeimg.com/400/150/arch',
+    title: 'Post title 1',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
+    link: 'link-1.com',
+  },
+  {
+    img: 'https://placeimg.com/400/150/nature',
+    title: 'Post title 2',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
+    link: 'link-2.com',
+  },
+  {
+    img: 'https://placeimg.com/400/150/arch',
+    title: 'Post title 3',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
+    link: 'link-3.com',
+  },
+];
